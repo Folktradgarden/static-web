bb64eac863065663bfb320f3e9689457eafc2caa
Kim Gustafsson <kim.gustafsson@tretton37.com>
2020-11-28 16:11:07 +0100

    Add plugin to support absolut path alias

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,21 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+const path = require("path");
+
 module.exports = {
   /* Your site config here */
-  plugins: ["gatsby-plugin-styled-components"],
+  plugins: [
+    "gatsby-plugin-styled-components",
+    {
+      resolve: 'gatsby-plugin-root-import',
+      options: {
+        src: path.join(__dirname, 'src'),
+        pages: path.join(__dirname, 'src/pages'),
+        components: path.join(__dirname, 'src/components'),
+        presentational: path.join(__dirname, 'src/components/presentational'),
+        styling: path.join(__dirname, 'src/styling'),
+      }
+    }
+  ],
 }
